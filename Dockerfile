354c14c7cfb4b0be817607f31f7a91b15e8a6660
浩会海 <50604324+haohuihai@users.noreply.github.com>
2022-07-09 20:55:35 +0800

    111

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM nginx
+LABEL name="vite-front"
+LABEL version="1.0"
+# COPY  ./dist/ /www/server/nginx/html
+# COPY ./vite-front.conf /www/server/panel/vhost/nginx
+COPY  ./dist/ /usr/share/nginx/html/
+COPY ./vite-front.conf /etc/nginx/conf.d/
+EXPOSE 3000
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in users" :key="item.id">{{ item.name }}</li>
+    </ul>
+    <div>welcome my world</div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  components: {},
+  props: {},
+  setup(props, context) {},
+  mounted() {
+    axios.get("http://101.132.124.188:10001/api/users").then((response) => {
+      this.users = response.data;
+    });
+  },
+};
+</script>
+<style scoped>
+</style>
