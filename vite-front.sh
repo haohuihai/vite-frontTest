5f1d1ff9a9e9b9d66473625b06ccec4f9c5123af
浩会海 <50604324+haohuihai@users.noreply.github.com>
2022-07-09 22:21:01 +0800

    111

--- vite-front.sh
+++ vite-front.sh
@@ -8,9 +8,9 @@ git pull origin main
 echo "打包最新代码"
 npm run build
 echo '开始构建镜像'
-docker build -t vite-frontTest:1.0 .
+docker build -t vite-fronttest:1.0 .
 echo '删除旧容器'
-docker stop vite-frontTest-container
-docker rm vite-frontTest-container
+docker stop vite-fronttest-container
+docker rm vite-fronttest-container
 echo '启动新容器'
-docker container run -p 3001:80 -d --name vite-frontTest-container vite-frontTest:1.0
+docker container run -p 3001:80 -d --name vite-fronttest-container vite-fronttest:1.0
